8cd169f72591a42656dec44443297345ba9a1c8b
AndriesDK <andriesd@cashcrusaders.co.za>
2025-03-05 18:14:32 +0200

    added routing pages

---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,16 @@
+﻿import {View, Text} from 'react-native'
+import React from 'react'
+import {useLocalSearchParams} from "expo-router";
+
+const Property = () => {
+
+    // gets the 'id' from the route parameter
+    const {id} = useLocalSearchParams()
+
+    return (
+        <View>
+            <Text>Property {id}</Text>
+        </View>
+    )
+}
+    export default Property
---
+++ app/sign-in.tsx
@@ -0,0 +1,11 @@
+﻿import {View, Text} from 'react-native'
+import React from 'react'
+
+const SignIn = () => {
+    return (
+        <View>
+            <Text>SignIn</Text>
+        </View>
+    )
+}
+export default SignIn
