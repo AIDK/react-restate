84d9a087c871b21f40e23a868e6794688b5af703
Andries de Kock <53447003+AIDK@users.noreply.github.com>
2025-03-06 10:13:49 +0200

    added navigation for home, explore and profile (#3)
    Co-authored-by: AndriesDK <andriesd@cashcrusaders.co.za>

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -10,11 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <Text className={"font-bold  my-10 font-rubik text-3xl"}>Welcome to ReState</Text>
-        <Link href={"/sign-in"}>Sign In</Link>
-        <Link href={"/explore"}>Explore</Link>
-        <Link href={"/profile"}>Profile</Link>
-        <Link href={"/properties/[id]"}>Property</Link>
+      <Text className={"font-bold  my-10 font-rubik text-3xl"}>
+        Welcome to ReState
+      </Text>
     </View>
   );
 }
