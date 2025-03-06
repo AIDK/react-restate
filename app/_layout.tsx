ba4af5ce92da8a5fcef9be819d10820bedc3cf51
AndriesDK <andriesd@cashcrusaders.co.za>
2025-03-06 11:34:19 +0200

    implemented profile screen with user avatar, menu settings and logout functionality

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout() {
 
   return (
     <GlobalProvider>
-      <Stack screenOptions={{ headerShown: false }} />;
+      <Stack screenOptions={{ headerShown: false }} />
     </GlobalProvider>
   );
 }
